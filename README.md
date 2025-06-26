4249c8e7a7108c4e175b98a48c59596a275338aa
nirav <niravgabani8787@gmail.com>
2025-06-26 11:58:26 +0530

    new

---
+++ README.md
@@ -0,0 +1,1 @@
+# gadhapurbus.github.io
--- src/gun/data/gunskins.php
+++
@@ -1,8 +0,0 @@
-<?php
-$gun = [
-  ["name" => "Gatling", "image" => "../../assets/images/gatling.png"],
-  ["name" => "M79", "image" => "../../assets/images/m79.png"],
-  ["name" => "MGL140", "image" => "../../assets/images/mgl140.png"],
-  ["name" => "RGS50", "image" => "../../assets/images/rgs50.png"]
-];
-?>
